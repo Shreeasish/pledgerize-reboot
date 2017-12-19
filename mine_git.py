a8c691ca4ea9b8addfc64f306fd94dd2da023a86
Shreeasish Kumar <shreeasish@gmail.com>
2017-12-18 17:44:50 -0800

    Retrieved first insertion id for each line. Need to get commit window

--- mine_git.py
+++ mine_git.py
@@ -6,8 +6,8 @@ from git import Repo
 PATH = os.path.abspath("/home/ska196/Pledges/references/src")
 REPO = Repo(PATH)
 assert not REPO.bare
-#For testing use 0:8
-lines = [line for line in REPO.git.g('pledge').split('\n')[0:8]]
+# For testing use 0:8
+lines = [line for line in REPO.git.g('pledge').split('\n')[0:62]]
 
 #%%
 pledge_files = []
@@ -19,6 +19,36 @@ for line in lines:
         temp_group = []
 
 #%%
-# How do I add multiple items in a single dict comprehension
-pledge_files_dict = {item[0]: x.split(":")[0] for item in pledge_files for x in item[1:] }
+# pledge_files_dict = {item[0]: x.split(":")[0] for item in pledge_files for x in item[1:] }
+pledge_files_dict = {}
+for items in pledge_files:
+    for line_number in items[1:]:
+        if items[0] not in pledge_files_dict:
+            pledge_files_dict[items[0]] = [line_number.split(":")[0]]
+        else:
+            pledge_files_dict[items[0]].append( line_number.split(":")[0] )
 print(pledge_files_dict)
+
+#%%
+'''
+pledges seem to always be inserted in two lines like this
++	if (tame("stdio rpath", NULL) == -1)
++		err(1, "tame");
+git log -L 216,217,bin/md5/md5.c
+'''
+
+for file_name in pledge_files_dict:
+    commit_set = set()
+    line_numbers = pledge_files_dict[file_name]
+    for idx in range(0,len(line_numbers),2):
+        print(line_numbers[idx] + "," + line_numbers[idx+1] + ":" + file_name)
+        '''
+        Get the last commit shown (earliest commit in for the line number). 
+        This should be the commit where it was inserted.
+        '''
+        final_commit = ""
+        for line in REPO.git.log( '-L ' + line_numbers[idx] + "," + line_numbers[idx+1] + ":" + file_name ).split("\n"):
+            if "commit" in line:
+                final_commit = line.split(" ")[1]
+        commit_set.add(final_commit)
+    print("COMMIT SET -- ", commit_set)
