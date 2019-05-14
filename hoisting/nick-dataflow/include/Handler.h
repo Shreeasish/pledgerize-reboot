2a3535bbf419f89f5dab123d519ea5ad787386ab
Shreeasish Kumar <shreeasish@gmail.com>
2019-05-13 19:50:11 -0700

    Support files for previous commit

---
+++ hoisting/nick-dataflow/include/Handler.h
@@ -0,0 +1,231 @@
+#ifndef FUTUREFUNCTIONS_H
+#define FUTUREFUNCTIONS_H
+
+#include "llvm/IR/CallSite.h"
+#include "llvm/IR/DebugInfo.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <bitset>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <variant>
+#include <functional>
+
+#include "TaintAnalysis.h"
+#include "PromiseDeclarations.h"
+#include "CallGraphAnalyzer.h"
+
+using Privileges = std::bitset<COUNT>;
+using Context    = std::array<llvm::Instruction*, 2ul>;
+
+struct AnalysisPackage {
+public:
+  // Point of Configuration for turning on or off analysis
+  AnalysisPackage(llvm::Module& m) { };
+private:
+  tmpanalysis::tmppathResultsTy tmppathResults;
+};
+
+
+class PrivilegeCheckerBase {
+public:
+  PrivilegeCheckerBase(int ap) : argposition{ap} {}
+  virtual ~PrivilegeCheckerBase() {}
+
+  virtual Privileges operator()(const llvm::CallSite,
+                                const Context& context,
+                                AnalysisPackage* package) = 0;
+
+  int
+  getArgPosition() {
+    return argposition;
+  }
+
+private:
+  int argposition;
+};
+
+using AnalysisVector = std::vector<std::unique_ptr<PrivilegeCheckerBase>>;
+class FunctionPrivileges {
+public:
+  FunctionPrivileges(unsigned long bitString) : privileges{bitString} {}
+
+  FunctionPrivileges(unsigned long bitString,
+                     AnalysisPackage* package,
+                     AnalysisVector avector)
+    : privileges{bitString},
+      analysisPackage{package},
+      analysisVector{std::move(avector)} {}
+
+  Privileges
+  getPrivileges(const llvm::CallSite& cs, const Context& context) {
+    for (auto const& checker : analysisVector) {
+      privileges |= (*checker)(cs, context, analysisPackage);
+    }
+    return privileges;
+  }
+
+private:
+  Privileges privileges;
+  AnalysisPackage* analysisPackage;
+  AnalysisVector analysisVector;
+};
+
+
+class FunctionPrivilegesBuilder {
+public:
+  FunctionPrivilegesBuilder(unsigned long privilegeValue)
+    : privileges{privilegeValue} {}
+
+  FunctionPrivilegesBuilder(unsigned long privilegeValue,
+                            AnalysisPackage* package)
+    : privileges{privilegeValue} {}
+
+  FunctionPrivilegesBuilder&
+  add(std::unique_ptr<PrivilegeCheckerBase> privilegeChecker) {
+    analysisVector.push_back(std::move(privilegeChecker));
+    return *this;
+  }
+
+  FunctionPrivileges
+  build() {
+    return FunctionPrivileges(
+        privileges.to_ulong(), analysisPackage, std::move(analysisVector));
+  }
+
+private:
+  Privileges privileges;
+  AnalysisVector analysisVector;
+  AnalysisPackage* analysisPackage;
+};
+
+using Handlers = llvm::StringMap<FunctionPrivileges>;
+struct LibCHandlersMap {
+public:
+  LibCHandlersMap(AnalysisPackage& package) {
+    buildLibCHandlers(package);
+  }
+  
+  auto
+  find(llvm::StringRef functionName) {
+    return libCHandlers.find(functionName);
+  }
+
+  auto
+  end() {
+    return libCHandlers.end();
+  }
+
+private:
+  void buildLibCHandlers(AnalysisPackage& package);
+  Handlers libCHandlers;
+};
+
+
+class PrivilegeResolver {
+public:
+  PrivilegeResolver(llvm::Module& m)
+    : module{m},
+      analysisPackage{std::make_unique<AnalysisPackage>(m)},
+      mapInterface{std::make_unique<MapInterface>(*analysisPackage.get())} { }
+
+  void
+  setPrivileges(Privileges& requiredPrivileges,
+                llvm::CallSite cs,
+                const Context& context) {
+    //auto* fun         = getCalledFunction(cs);
+    //auto functionName = fun->getName().str();
+    //auto found        = mapInterface.find(functionName);
+
+    //if (found != libCHandlers.end()) {
+
+    //}
+
+    //auto privileges = found->second.getPrivileges(cs, context);
+    //requiredPrivileges |= privileges;
+    return;
+  }
+
+  template <Promises promise>
+  bool
+  hasPrivilege(llvm::CallSite cs, const Context& context) {
+    Privileges privs{};
+    setPrivileges(privs, cs, context);
+    return Privileges{promise} == privs;
+  }
+
+private:
+  LibCHandlersMap& buildLibCHandlers(AnalysisPackage&); // Handler.cpp
+  
+  static
+  llvm::Function*
+  getCalledFunction(llvm::CallSite cs) {
+    auto* calledValue = cs.getCalledValue()->stripPointerCasts();
+    return llvm::dyn_cast<llvm::Function>(calledValue);
+  }
+
+  class MapInterface {
+  public:
+    MapInterface(AnalysisPackage& package) 
+      : libCHandlers{package} {}
+
+    std::bitset<COUNT>
+    getPrivilegesFor(const llvm::CallSite& cs, const Context& context) {
+      // Point of configuration for specifications
+      if (auto privileges = getPrivilegesForImpl(cs, context,syscallBitsetMap)) {
+        return *privileges;
+      } else if (auto privileges =
+                     getPrivilegesForImpl(cs, context, libCHandlers)) {
+        return *privileges;
+      } else {
+        llvm::errs() << "\nNo function found for CallSite "
+                     << *cs.getInstruction();
+        llvm_unreachable("FucK");
+      }
+    }
+
+  private:
+    template <typename StringMapTy>
+    std::optional<Privileges>
+    getPrivilegesForImpl(const llvm::CallSite& cs, const Context& context,
+                                                    StringMapTy stringMap) {
+      auto functionName = getFunctionName(cs);
+      return stringMap.find(functionName) == stringMap.end()
+                 ? std::optional{stringMap.find(functionName)->second}
+                 : std::nullopt;
+    }
+
+    std::optional<Privileges>
+    getPrivilegesForImpl(const llvm::CallSite& cs, const Context& context,
+                                                LibCHandlersMap& handlers) {
+      auto functionName = getFunctionName(cs);
+      return handlers.find(functionName) == handlers.end()
+                 ? std::optional{handlers.find(functionName)
+                                     ->second.getPrivileges(cs, context)}
+                 : std::nullopt;
+    }
+
+    llvm::StringRef
+    getFunctionName(const llvm::CallSite& cs) const {
+      return getCalledFunction(cs)->getName();
+    }
+
+    LibCHandlersMap libCHandlers;
+  };
+
+  llvm::Module& module;
+  std::unique_ptr<AnalysisPackage> analysisPackage;
+  std::unique_ptr<MapInterface>    mapInterface;
+};
+
+// std::unordered_map<std::string, FunctionPledges>
+#endif
---
+++ hoisting/nick-dataflow/include/InstructionResolver.h
@@ -0,0 +1,98 @@
+#ifndef INSTRUCTIONRESOLVER_H
+#define INSTRUCTIONRESOLVER_H
+
+#include "ConditionList.h"
+#include "Generator.h"
+
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "CustomDataflowAnalysis.h"
+
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <vector>
+#include <deque>
+
+namespace lowering {
+  class InstructionResolver;
+  struct LocationState;
+}
+
+using InstructionResolver = lowering::InstructionResolver;
+
+struct lowering::LocationState {
+public:
+  const Context& context;
+  llvm::Instruction* const parentCallSite;
+  llvm::Function* const callee;
+  llvm::Instruction* const location;
+  const Disjunction& state; /*state[nullptr]*/
+};
+
+class lowering::InstructionResolver {
+public:
+  InstructionResolver(const llvm::Module& m, Generator* const g) 
+    : module{m}, generator{g} { }
+
+  Disjunction
+  operator()(const LocationState& locationState) {
+    auto toFix = getOutOfScopeValues(locationState);
+    return locationState.state; //!!! Remove
+  }
+  
+private:
+  const llvm::Module& module;
+  Generator* const generator;
+  struct OutOfScopeFinder;
+
+  std::vector<llvm::Value*>
+  getOutOfScopeValues(const LocationState& locationState) const;
+};
+
+
+struct InstructionResolver::OutOfScopeFinder {
+public:
+  OutOfScopeFinder(const llvm::Function* parent, std::vector<llvm::Value*>& retVec) 
+    : locationParent{parent}, outOfScopeValues{retVec} {}
+
+  void
+  operator()(ExprID id, ConstantExprNode node) {
+    return;
+  }
+
+  void
+  operator()(ExprID id, BinaryExprNode node) {
+    return;
+  }
+
+  void
+  operator()(ExprID id, ValueExprNode node) {
+    if (!node.value) {
+      return;
+    }
+    auto* asInst = llvm::dyn_cast<llvm::Instruction>(node.value);
+    const llvm::Function* valueParent = asInst->getParent()->getParent();
+    if (valueParent != locationParent) {
+      outOfScopeValues.emplace_back(node.value);
+   }
+    return;
+  }
+  
+
+private:
+  const llvm::Function* locationParent;
+  std::vector<llvm::Value*>& outOfScopeValues;
+};
+
+std::vector<llvm::Value*>
+InstructionResolver::getOutOfScopeValues(const LocationState& locationState) const {
+  const llvm::Function* loweringFunction =
+      locationState.location->getFunction();
+  std::vector<llvm::Value*> outOfScopeValues;
+  auto visitor = OutOfScopeFinder{loweringFunction, outOfScopeValues};
+  generator->preOrderFor(locationState.state, visitor);
+  return outOfScopeValues;
+}
+
+#endif
